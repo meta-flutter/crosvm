a3a888a0c1a6e0622208bf9ea88f0584f7d77d4c
Daniel Verkamp <dverkamp@chromium.org>
2022-06-02 16:44:06 +0000

    Revert "devices: pci: PciBarIndex API cleanup"
    This reverts commit e84d9c1d7c68782bfbdcffc831d6f8a069abfde2.
    
    Reason for revert: vfio-pci fails on ManaTEE with an out-of-range BAR index (8), so further investigation is required
    
    Original change's description:
    > devices: pci: PciBarIndex API cleanup
    >
    > Everything that takes a PCI BAR index should use the appropriate
    > PciBarIndex type rather than u8 (or usize) for better type safety and
    > descriptiveness.
    >
    > BUG=b:232838930
    > TEST=tools/presubmit
    >
    > Change-Id: I0a918a6da6327c963fb297ba765563774a7e5e49
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3664282
    > Tested-by: kokoro <noreply+kokoro@google.com>
    > Commit-Queue: Daniel Verkamp <dverkamp@chromium.org>
    > Reviewed-by: Anton Romanov <romanton@google.com>
    
    Bug: b:232838930
    Change-Id: Id5d19e779289e47045af67fa51c301ba2b79ee7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3687411
    Reviewed-by: Anton Romanov <romanton@google.com>
    Tested-by: Dennis Kempin <denniskempin@google.com>
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Daniel Verkamp <dverkamp@chromium.org>

--- devices/src/virtio/gpu/mod.rs
+++ devices/src/virtio/gpu/mod.rs
@@ -54,8 +54,7 @@ pub use self::protocol::{
 use self::virtio_gpu::VirtioGpu;
 
 use crate::pci::{
-    PciAddress, PciBarConfiguration, PciBarIndex, PciBarPrefetchable, PciBarRegionType,
-    PciCapability,
+    PciAddress, PciBarConfiguration, PciBarPrefetchable, PciBarRegionType, PciCapability,
 };
 
 pub const DEFAULT_DISPLAY_WIDTH: u32 = 1280;
@@ -106,7 +105,7 @@ pub struct GpuParameters {
 pub const QUEUE_SIZES: &[u16] = &[256, 16];
 pub const FENCE_POLL_INTERVAL: Duration = Duration::from_millis(1);
 
-pub const GPU_BAR_NUM: PciBarIndex = PciBarIndex::Bar4;
+pub const GPU_BAR_NUM: u8 = 4;
 pub const GPU_BAR_OFFSET: u64 = 0;
 pub const GPU_BAR_SIZE: u64 = 1 << 33;
 
@@ -1378,10 +1377,10 @@ impl VirtioDevice for Gpu {
             bus: address.bus,
             dev: address.dev,
             func: address.func,
-            bar: GPU_BAR_NUM.into(),
+            bar: GPU_BAR_NUM,
         });
         vec![PciBarConfiguration::new(
-            GPU_BAR_NUM,
+            GPU_BAR_NUM as usize,
             GPU_BAR_SIZE,
             PciBarRegionType::Memory64BitRegion,
             PciBarPrefetchable::NotPrefetchable,

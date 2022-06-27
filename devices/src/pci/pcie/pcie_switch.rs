63da69ea5ca82f19525315f0a94875f6dec0ca72
Dennis Kempin <denniskempin@google.com>
2022-06-27 19:47:30 +0000

    Revert "devices: pci: don't reserve bridge window for hot added bridges"
    This reverts commit b6ef09ecc582f6069f9186fcafaf6280d8770b8e.
    
    Change-Id: I09e27512003a8a4f81505c02b650962c4160ded8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/crosvm/+/3727472
    Commit-Queue: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Maciek Swiech <drmasquatch@google.com>
    Tested-by: Dennis Kempin <denniskempin@google.com>
    Reviewed-by: Dennis Kempin <denniskempin@google.com>

--- devices/src/pci/pcie/pcie_switch.rs
+++ devices/src/pci/pcie/pcie_switch.rs
@@ -2,11 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-use std::sync::Arc;
-use sync::Mutex;
-
 use crate::pci::pci_configuration::PciCapabilityID;
-use crate::pci::{MsiConfig, PciAddress, PciCapability, PciDeviceError};
+use crate::pci::{PciAddress, PciCapability, PciDeviceError};
 
 use crate::pci::pcie::pci_bridge::PciBridgeBusRange;
 use crate::pci::pcie::pcie_device::{PciPmcCap, PcieCap, PcieDevice};
@@ -20,12 +17,11 @@ const PCIE_DP_DID: u16 = 0x3510;
 
 pub struct PcieUpstreamPort {
     pcie_port: PciePort,
-    hotplugged: bool,
 }
 
 impl PcieUpstreamPort {
     /// Constructs a new PCIE upstream port
-    pub fn new(primary_bus_num: u8, secondary_bus_num: u8, hotplugged: bool) -> Self {
+    pub fn new(primary_bus_num: u8, secondary_bus_num: u8) -> Self {
         PcieUpstreamPort {
             pcie_port: PciePort::new(
                 PCIE_UP_DID,
@@ -34,14 +30,12 @@ impl PcieUpstreamPort {
                 secondary_bus_num,
                 false,
             ),
-            hotplugged,
         }
     }
 
-    pub fn new_from_host(pcie_host: PcieHostPort, hotplugged: bool) -> Self {
+    pub fn new_from_host(pcie_host: PcieHostPort) -> Self {
         PcieUpstreamPort {
             pcie_port: PciePort::new_from_host(pcie_host, false),
-            hotplugged,
         }
     }
 }
@@ -60,10 +54,6 @@ impl PcieDevice for PcieUpstreamPort {
         resources: &mut SystemAllocator,
     ) -> std::result::Result<PciAddress, PciDeviceError> {
         self.pcie_port.allocate_address(resources)
-    }
-
-    fn clone_interrupt(&mut self, msi_config: Arc<Mutex<MsiConfig>>) {
-        self.pcie_port.clone_interrupt(msi_config);
     }
 
     fn read_config(&self, reg_idx: usize, data: &mut u32) {
@@ -89,16 +79,8 @@ impl PcieDevice for PcieUpstreamPort {
         self.pcie_port.get_bus_range()
     }
 
-    fn get_removed_devices(&self) -> Vec<PciAddress> {
-        Vec::new()
-    }
-
     fn hotplug_implemented(&self) -> bool {
         false
-    }
-
-    fn hotplugged(&self) -> bool {
-        self.hotplugged
     }
 
     fn get_bridge_window_size(&self) -> (u64, u64) {
@@ -108,12 +90,11 @@ impl PcieDevice for PcieUpstreamPort {
 
 pub struct PcieDownstreamPort {
     pcie_port: PciePort,
-    hotplugged: bool,
 }
 
 impl PcieDownstreamPort {
     /// Constructs a new PCIE downstream port
-    pub fn new(primary_bus_num: u8, secondary_bus_num: u8, hotplugged: bool) -> Self {
+    pub fn new(primary_bus_num: u8, secondary_bus_num: u8) -> Self {
         PcieDownstreamPort {
             pcie_port: PciePort::new(
                 PCIE_DP_DID,
@@ -122,14 +103,12 @@ impl PcieDownstreamPort {
                 secondary_bus_num,
                 false,
             ),
-            hotplugged,
         }
     }
 
-    pub fn new_from_host(pcie_host: PcieHostPort, hotplugged: bool) -> Self {
+    pub fn new_from_host(pcie_host: PcieHostPort) -> Self {
         PcieDownstreamPort {
             pcie_port: PciePort::new_from_host(pcie_host, false),
-            hotplugged,
         }
     }
 }
@@ -148,10 +127,6 @@ impl PcieDevice for PcieDownstreamPort {
         resources: &mut SystemAllocator,
     ) -> std::result::Result<PciAddress, PciDeviceError> {
         self.pcie_port.allocate_address(resources)
-    }
-
-    fn clone_interrupt(&mut self, msi_config: Arc<Mutex<MsiConfig>>) {
-        self.pcie_port.clone_interrupt(msi_config);
     }
 
     fn read_config(&self, reg_idx: usize, data: &mut u32) {
@@ -177,16 +152,8 @@ impl PcieDevice for PcieDownstreamPort {
         self.pcie_port.get_bus_range()
     }
 
-    fn get_removed_devices(&self) -> Vec<PciAddress> {
-        Vec::new()
-    }
-
     fn hotplug_implemented(&self) -> bool {
         false
-    }
-
-    fn hotplugged(&self) -> bool {
-        self.hotplugged
     }
 
     fn get_bridge_window_size(&self) -> (u64, u64) {
